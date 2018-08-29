8e15e4a6f77a38ebe603bbb588bfa737d7ee3835
hyhhhhhh <42395923+hyhhhhhh@users.noreply.github.com>
2018-08-29 14:57:23 +0800

    Update gotest_test.go

--- gotest_test.go
+++ gotest_test.go
@@ -13,5 +13,9 @@ func Test_Division_1(t *testing.T) {
 }
 
 func Test_Division_2(t *testing.T) {
-    t.Error("就是不通过")
-}
+    if _, e := Division(6, 0); e == nil { //try a unit test on function
+        t.Error("Division did not work as expected.") // 如果不是如预期的那么就报错
+    } else {
+        t.Log("one test passed.", e) //记录一些你期望记录的信息
+    }
+}   
